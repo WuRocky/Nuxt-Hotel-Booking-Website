805ce936d363ec59954629051849b0e94641d9b4
Rocky Wu <rocky_wu@everrich.com.tw>
2024-11-04 10:52:29 +0800

    create index task

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const route = useRoute()
+</script>
+
+<template>
+  <div>
+    <h1>
+      Page: index
+    </h1>
+    <div>
+      目前路由的路徑 - route.fullPath 為: {{ route.fullPath }}
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
